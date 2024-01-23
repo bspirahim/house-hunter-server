23d153b46e5bbbac4090f56ace940324363ccb9b
Abdur-P-Rahim <bspirahim@gmail.com>
2024-01-24 02:28:06 +0600

    bug fixed

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const CreateJWT = function (user) {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
 
         const houseCollection = client.db('houseDB').collection('houseOwner');
         const usersCollection = client.db('houseDB').collection('users');
@@ -149,8 +149,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
